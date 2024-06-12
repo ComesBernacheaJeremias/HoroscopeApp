111b8ebeb331c44a93b47be5544487e183ae33f0
Jeremías <jeremias4810@hotmail.com>
2024-06-12 13:13:11 -0300

    add imagenes y HoroscopeInfo

---
+++ app/src/main/java/com/example/horoscapp/domain/model/HoroscopeInfo.kt
@@ -0,0 +1,19 @@
+package com.example.horoscapp.domain.model
+
+import com.example.horoscapp.R
+
+sealed class HoroscopeInfo (val img:Int, val name:Int){
+    data object Aries:HoroscopeInfo(R.drawable.aries, R.string.aries)
+    data object Aquario:HoroscopeInfo(R.drawable.aquario,R.string.aquarius)
+    data object Cancer:HoroscopeInfo(R.drawable.cancer,R.string.cancer)
+    data object Capricornio:HoroscopeInfo(R.drawable.capricornio,R.string.capricorn)
+    data object Escorpio:HoroscopeInfo(R.drawable.escorpio,R.string.scorpio)
+    data object Geminis:HoroscopeInfo(R.drawable.geminis,R.string.gemini)
+    data object Leo:HoroscopeInfo(R.drawable.leo,R.string.leo)
+    data object Libra:HoroscopeInfo(R.drawable.libra,R.string.libra)
+    data object Piscis:HoroscopeInfo(R.drawable.piscis,R.string.pisces)
+    data object Sagitario:HoroscopeInfo(R.drawable.sagitario,R.string.sagittarius)
+    data object Tauro:HoroscopeInfo(R.drawable.tauro,R.string.taurus)
+    data object Virgo:HoroscopeInfo(R.drawable.virgo,R.string.virgo)
+
+}
--- app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopoFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopoFragment.kt
@@ -1,19 +1,44 @@
 package com.example.horoscapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.horoscapp.R
 import com.example.horoscapp.databinding.FragmentHoroscopoBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopoFragment : Fragment() {
+    private val horoscopoViewModel by viewModels <HoroscopoViewModel>()
 
     private var _binding: FragmentHoroscopoBinding? = null
     private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+    private fun initUI(){
+        initUIState()
+    }
+    private fun initUIState(){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                horoscopoViewModel.horoscope.collect{
+                    Log.i("corcho", it.toString())
+                }
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
